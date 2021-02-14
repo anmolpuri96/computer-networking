42c4b369fed9cee14b20742a3631d5ace9ff9d67
Anmol Puri <anmolpuri96@gmail.com>
2021-02-14 18:00:39 -0500

    testing with new header format

--- solution.py
+++ solution.py
@@ -20,11 +20,11 @@ def webServer(port=13331):
 			outputdata = f.read()
 
 			#Send one HTTP header line into socket
-			# header = "HTTP/1.1 200 OK\r\n\r\n"
+			header = "HTTP/1.1 200 OK\r\n\r\n"
 			protocol = 'HTTP/1.1'
 			status = '200'
 			text = 'OK'
-			connectionSocket.send(f"{protocol} {status} {text}".encode())
+			connectionSocket.send(header.encode())
 
 			#Send the content of the requested file to the client
 			for i in range(0, len(outputdata)):
