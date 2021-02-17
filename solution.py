55a9da0785c29eb7652c262f76f21069dc99382b
Anmol Puri <anmolpuri96@gmail.com>
2021-02-16 21:03:51 -0500

    Initial test

--- solution.py
+++ solution.py
@@ -1,32 +1,78 @@
-### welcome_assignment_answers
-### Input - All eight questions given in the assignment.
-### Output - The right answer for the specific question.
-
-def welcome_assignment_answers(question):
-    #The student doesn't have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = "42b76fe51778764973077a5a94056724"
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 5
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 4
-
-    return(answer)
-# Complete all the questions.
+from socket import *
 
 
-if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
+def smtp_client(port=1025, mailserver='127.0.0.1'):
+    msg = "\r\n My message"
+    endmsg = "\r\n.\r\n"
+
+    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
+    # mailserver = "smtp.gmail.com"
+    # port = 25
+
+    # Create socket called clientSocket and establish a TCP connection with mailserver and port
+
+    # Fill in start
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, port))
+    # Fill in end
+
+    recv = clientSocket.recv(1024).decode()
+    print(recv)
+    if recv[:3] != '220':
+        print('220 reply not received from server.')
+
+    # Send HELO command and print server response.
+    heloCommand = 'HELO Alice\r\n'
+    clientSocket.send(heloCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
+    print(recv1)
+    if recv1[:3] != '250':
+        print('250 reply not received from server.')
+
+    # Send MAIL FROM command and print server response.
+    # Fill in start
+    clientSocket.send(b"MAIL From: ap4058@nyu.edu\r\n")
+    recv2 = clientSocket.recv(1024).decode()
+    print(recv2)
+    # Fill in end
+
+    # Send RCPT TO command and print server response.
+    # Fill in start
+    clientSocket.send(b"RCPT TO: anmolpuri96@gmail.com\r\n")
+    recv3 = clientSocket.recv(1024).decode()
+    print(recv3)
+    # Fill in end
+
+    # Send DATA command and print server response.
+    # Fill in start
+    clientSocket.send(b"DATA\r\n")
+    recv4 = clientSocket.recv(1024).decode()
+    print(recv4)
+    # Fill in end
+
+    # Send message data.
+    # Fill in start
+    # subject = "Python SMTP lab"
+    # body = "Testing my smtp python lab"
+    clientSocket.send(b"SUBJECT: Python SMTP lab\n"+"Testing my smtp python lab".encode('utf-8')+b"\n.\n\r\n\r\n")
+    recv5 = clientSocket.recv(1024).decode()
+    print(recv5)
+    # Fill in end
+
+    # # Message ends with a single period.
+    # # Fill in start
+    # clientSocket.send(b"MAIL From: testmail@mail.com\r\n")
+    # recv6 = clientSocket.recv(1024).decode()
+    # print(recv6)
+    # # Fill in end
+
+    # Send QUIT command and get server response.
+    # Fill in start
+    clientSocket.send(b"QUIT\r\n")
+    recv7 = clientSocket.recv(1024).decode()
+    print(recv7)
+    # Fill in end
+
+
+if __name__ == '__main__':
+    smtp_client(1025, '127.0.0.1')
