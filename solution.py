8eb43021a0238ed1c9fbf1d0ed35fd83a1daa492
Anmol Puri <anmolpuri96@gmail.com>
2021-03-05 17:46:30 -0500

    Another fix

--- solution.py
+++ solution.py
@@ -103,31 +103,31 @@ def get_route(hostname):
                 whatReady = select.select([mySocket], [], [], timeLeft)
                 howLongInSelect = (time.time() - startedSelect)
                 if whatReady[0] == []: # Timeout
-                    # tracelist1.append(ttl)
-                    # tracelist1.append("*")
-                    # tracelist1.append("Request timed out")
-                    # tracelist2.append(tracelist1)
-                    # tracelist1 = []
-                    tracelist1.append("* * * Request timed out.")
-                    #You should add the list above to your all traces list
+                    tracelist1.append(f"ttl")
+                    tracelist1.append("*")
+                    tracelist1.append("Request timed out")
                     tracelist2.append(tracelist1)
                     tracelist1 = []
+                    # tracelist1.append("* * * Request timed out.")
+                    #You should add the list above to your all traces list
+                    # tracelist2.append(tracelist1)
+                    # tracelist1 = []
                     #Fill in start
                     #Fill in end
                 recvPacket, addr = mySocket.recvfrom(1024)
                 timeReceived = time.time()
                 timeLeft = timeLeft - howLongInSelect
                 if timeLeft <= 0:
-                    tracelist1.append("* * * Request timed out.")
-                    # tracelist1.append(ttl)
-                    # tracelist1.append("*")
-                    # tracelist1.append("Request timed out")
-                    # tracelist2.append(tracelist1)
-                    # tracelist1 = []
-                    #Fill in start
-                    #You should add the list above to your all traces list
+                    # tracelist1.append("* * * Request timed out.")
+                    tracelist1.append(f"ttl")
+                    tracelist1.append("*")
+                    tracelist1.append("Request timed out")
                     tracelist2.append(tracelist1)
                     tracelist1 = []
+                    #Fill in start
+                    #You should add the list above to your all traces list
+                    # tracelist2.append(tracelist1)
+                    # tracelist1 = []
                     #Fill in end
             except timeout:
                 continue
